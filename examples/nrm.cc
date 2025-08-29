50da9d6157919e208a6d29b484e197ad55c4b0d2
cauchy2b <841907997@qq.com>
2025-08-30 01:30:41 +0800

    [feat] enhance pk_nrm2 performance
    Persistent Nrm2 Kernel couldn't be faster than normal kernel,
    And it will accumulate error when vector size is large.

--- examples/nrm.cc
+++ examples/nrm.cc
@@ -1,12 +1,12 @@
-#include "lib/rocblas.hh"
 #include <3rdparty/argparse.hpp>
+#include <lib/rocblas.hh>
 #include <tools/helper.hh>
+#include <tools/intrinsic.hh>
 #include <tools/primitive.hh>
 #include <vector>
 
-std::vector<float> h_in;
-std::vector<float> h_out;
-float *d_in;
+std::vector<float> h_data;
+std::vector<float> h_ref;
 float *d_out;
 float *d_reduce_buf;
 
@@ -17,72 +17,83 @@ int warmup_runs = 5;
 constexpr int ThreadsPerBlock = 256;
 int NUM_SM                    = 0;
 
-template <typename T> void normalize(T *x, T *y, size_t n) {
+template <typename T> void normalize(T *x, size_t n) {
     T nrm2 = 0;
     for (size_t i = 0; i < n; i++) {
         nrm2 += x[i] * x[i];
     }
     nrm2 = std::sqrt(nrm2);
     for (size_t i = 0; i < n; i++) {
-        y[i] = x[i] / nrm2;
-    }
-}
-
-template <int GroupSize, typename T> __device__ __forceinline__ void warp_reduce_sum(T &val) {
-#pragma unroll
-    for (int offset = GroupSize / 2; offset > 0; offset /= 2) {
-        val += __shfl_down(val, offset);
+        x[i] /= nrm2;
     }
 }
 
 /// @brief Persistent kernel to perform Reduce + Axpy for normalization
 template <int ThreadsPerBlock = 256>
-__global__ void pkNormalize(const float *in, float *out, int N, float *g_reduce_buf) {
-    namespace cg            = cooperative_groups;
-    constexpr auto NumWarps = ThreadsPerBlock / warpSize;
+__global__ __launch_bounds__(ThreadsPerBlock) void pkNormalize(float *v, int N,
+                                                               float *g_reduce_buf) {
+    namespace cg               = cooperative_groups;
+    constexpr auto NumWarps    = ThreadsPerBlock / warpSize;
+    constexpr auto master_lane = warpSize - 1;
+    constexpr auto VecSize     = 4;
+    typedef float vXf32 __attribute__((ext_vector_type(VecSize)));
 
     __shared__ float smem_reduce[NumWarps];
+    __shared__ float smem_v[12 * 1024];
 
     auto grid    = cg::this_grid();
-    auto tid     = threadIdx.x;
     auto bid     = blockIdx.x;
-    auto lane_id = __lane_id();
+    auto tid     = threadIdx.x;
     auto warp_id = tid / warpSize;
+    auto lane_id = __lane_id();
 
     auto numel = divUp(N, gridDim.x);
-    auto start = bid * numel;
-    auto end   = min(start + numel, N);
 
-    /// Step 1: calculate nrm2
-    float sum = 0.0f;
-    for (int i = start + tid; i < end; i += ThreadsPerBlock) {
-        sum += in[i] * in[i];
+    float sum[VecSize] = {0.0f};
+    float r_v[VecSize];
+
+    BufferResource<float> buf_v(v, N * sizeof(float));
+    auto voffset = tid * VecSize;
+    auto start   = bid * numel;
+    /// Step 0: load data to shared memory and compute local square sum
+    for (int i = 0; i < numel; i += ThreadsPerBlock * VecSize) {
+        *reinterpret_cast<vXf32 *>(r_v) =
+            buf_v.load_x4(voffset * sizeof(float), (start + i) * sizeof(float), 0);
+#pragma unroll
+        for (int j = 0; j < VecSize; j++) {
+            sum[j] += r_v[j] * r_v[j]; // square for nrm
+        }
+        *reinterpret_cast<vXf32 *>(smem_v + i + voffset) = *reinterpret_cast<vXf32 *>(r_v);
     }
-    warp_reduce_sum<warpSize>(sum);
-    if (lane_id == 0) {
-        smem_reduce[warp_id] = sum;
+    warp_reduce_sum_dpp(sum);
+    if (lane_id == master_lane) {
+        float acc = 0.0f;
+        for (int i = 0; i < VecSize; i++) {
+            acc += sum[i]; // square sum for nrm2
+        }
+        smem_reduce[warp_id] = acc;
     }
     __syncthreads();
     if (tid == 0) {
-        sum = 0.0f;
+        float acc = 0.0f;
         for (int i = 0; i < NumWarps; i++) {
-            sum += smem_reduce[i];
+            acc += smem_reduce[i];
         }
-        g_reduce_buf[bid] = sum;
+        g_reduce_buf[bid] = acc;
     }
 
     grid.sync();
 
     __shared__ float s_nrm2;
-    float square_nrm2 = 0.0f;
-    float val         = tid < gridDim.x ? g_reduce_buf[tid] : 0.0f;
-    warp_reduce_sum<NumWarps>(val);
-    if (lane_id == 0) {
-        smem_reduce[warp_id] = val;
+    float val[1];
+    val[0] = tid < gridDim.x ? g_reduce_buf[tid] : 0.0f;
+    warp_reduce_sum_dpp(val);
+    if (lane_id == master_lane) {
+        smem_reduce[warp_id] = val[0];
     }
     __syncthreads();
     if (tid == 0) {
-        square_nrm2 = 0.0f;
+        float square_nrm2 = 0.0f;
         for (int i = 0; i < NumWarps; i++) {
             square_nrm2 += smem_reduce[i];
         }
@@ -91,13 +102,20 @@ __global__ void pkNormalize(const float *in, float *out, int N, float *g_reduce_
     __syncthreads();
     // Step 2: element-wise normalization
     float alpha = 1.0f / s_nrm2;
-    for (int i = start + tid; i < end; i += ThreadsPerBlock) {
-        out[i] = in[i] * alpha;
+    for (int i = 0; i < numel; i += ThreadsPerBlock * VecSize) {
+        *reinterpret_cast<vXf32 *>(r_v) = *reinterpret_cast<vXf32 *>(smem_v + i + voffset);
+#pragma unroll
+        for (int j = 0; j < VecSize; j++) {
+            r_v[j] *= alpha;
+        }
+        buf_v.store_x4(*reinterpret_cast<vXf32 *>(r_v), voffset * sizeof(float),
+                       (start + i) * sizeof(float), 0);
     }
 }
 
 template <bool shfl, int ThreadsPerBlock = 256, int VecSize = 1>
-__global__ void kReduceIntraBlock(float *in, float *g_reduce_buf, int N) {
+__global__ __launch_bounds__(ThreadsPerBlock) void kReduceIntraBlock(float *in, float *g_reduce_buf,
+                                                                     int N) {
     typedef float vXf32 __attribute__((ext_vector_type(VecSize)));
     constexpr auto NumWarps = ThreadsPerBlock / warpSize;
     __shared__ float smem_reduce[NumWarps];
@@ -137,7 +155,8 @@ __global__ void kReduceIntraBlock(float *in, float *g_reduce_buf, int N) {
 }
 
 template <bool shfl, int ThreadsPerBlock = 256, int VecSize = 1>
-__global__ void kReduceInterBlock(float *g_reduce_buf, float *out, int num_blocks) {
+__global__ __launch_bounds__(ThreadsPerBlock) void kReduceInterBlock(float *g_reduce_buf,
+                                                                     float *out, int num_blocks) {
     typedef float vXf32 __attribute__((ext_vector_type(VecSize)));
     constexpr auto NumWarps = ThreadsPerBlock / warpSize;
 
@@ -176,48 +195,31 @@ __global__ void kReduceInterBlock(float *g_reduce_buf, float *out, int num_block
     }
 }
 
-template <int ThreadsPerBlock, int VecSize = 1>
-__global__ void kSax(const float *in, float *out, size_t size, float *alpha) {
+template <int ThreadsPerBlock>
+__global__ __launch_bounds__(ThreadsPerBlock) void kSax(float *in, size_t size, float *alpha) {
+    constexpr auto VecSize = 4;
     typedef float vXf32 __attribute__((ext_vector_type(VecSize)));
-    auto tid     = blockIdx.x * blockDim.x + threadIdx.x;
+
+    BufferResource<float> buf_in(in, size * sizeof(float));
+    auto tid     = blockIdx.x * ThreadsPerBlock + threadIdx.x;
     auto alpha_v = 1.0f / *alpha;
-    if (tid * VecSize < size) {
-        float r_in[VecSize], r_out[VecSize];
-        *reinterpret_cast<vXf32 *>(r_in) = *reinterpret_cast<const vXf32 *>(in + tid * VecSize);
+    float r_in[VecSize], r_out[VecSize];
+    *reinterpret_cast<vXf32 *>(r_in) = buf_in.load_x4(tid * VecSize * sizeof(float), 0, 0);
 #pragma unroll
-        for (int j = 0; j < VecSize; j++) {
-            r_out[j] = alpha_v * r_in[j];
-        }
-        *reinterpret_cast<vXf32 *>(out + tid * VecSize) = *reinterpret_cast<const vXf32 *>(r_out);
+    for (int j = 0; j < VecSize; j++) {
+        r_out[j] = alpha_v * r_in[j];
     }
+    buf_in.store_x4(*reinterpret_cast<vXf32 *>(r_out), tid * VecSize * sizeof(float), 0, 0);
 }
 
-template <bool shfl, int VecSize>
-void launch_nrm2_multi_phase(const float *in, float *out, int N, float *reduce_buf) {
+template <bool shfl, int VecSize> void launch_nrm2_multi_phase(float *v, int N, float *reduce_buf) {
     int num_blocks = divUp(N, ThreadsPerBlock * VecSize);
     kReduceIntraBlock<shfl, ThreadsPerBlock, VecSize>
-        <<<num_blocks, ThreadsPerBlock>>>(const_cast<float *>(in), reduce_buf, N);
+        <<<num_blocks, ThreadsPerBlock>>>(const_cast<float *>(v), reduce_buf, N);
     kReduceInterBlock<shfl, ThreadsPerBlock, VecSize>
         <<<1, ThreadsPerBlock>>>(reduce_buf, reduce_buf, num_blocks); // write to buf[0]
-    kSax<ThreadsPerBlock, VecSize>
-        <<<divUp(N, ThreadsPerBlock * VecSize), ThreadsPerBlock>>>(in, out, N, reduce_buf);
-}
-
-void run_baseline() {
-    CPUTimer timer;
-    normalize(h_in.data(), h_out.data(), h_in.size());
-    for (int i = 0; i < warmup_runs; i++) {
-        normalize(h_in.data(), h_out.data(), h_in.size());
-    }
-    float ms = 0;
-    for (int i = 0; i < timed_runs; i++) {
-        timer.start();
-        normalize(h_in.data(), h_out.data(), h_in.size());
-        timer.stop();
-        ms += timer.milliseconds();
-    }
-    printf("Baseline: %f ms\n", ms / timed_runs);
-    std::cout << std::endl;
+    kSax<ThreadsPerBlock>
+        <<<divUp(N, ThreadsPerBlock * VecSize), ThreadsPerBlock>>>(v, N, reduce_buf);
 }
 
 int parse_args(int argc, char *argv[]) {
@@ -255,60 +257,58 @@ int parse_args(int argc, char *argv[]) {
 int main(int argc, char *argv[]) {
     parse_args(argc, argv);
 
-    h_in.resize(N);
-    h_out.resize(N);
+    h_data.resize(N);
+    h_ref.resize(N);
     Randomizer<float> rand(-1.0f, 1.0f);
-    rand.fill_random(h_in.data(), h_in.size());
-    run_baseline();
+    rand.fill_random(h_data.data(), h_data.size());
+    memcpy(h_ref.data(), h_data.data(), N * sizeof(float));
+    normalize(h_ref.data(), h_ref.size());
 
     {
-        check_runtime_api(hipMalloc(&d_in, N * sizeof(float)));
         check_runtime_api(hipMalloc(&d_out, N * sizeof(float)));
         check_runtime_api(hipMalloc(&d_reduce_buf, N * sizeof(float)));
-        check_runtime_api(hipMemcpy(d_in, h_in.data(), N * sizeof(float), hipMemcpyHostToDevice));
+        check_runtime_api(
+            hipMemcpy(d_out, h_data.data(), N * sizeof(float), hipMemcpyHostToDevice));
     }
 
-    size_t bytes = 2 * N * sizeof(float);
-    size_t flops = 3 * N; // 2 * N (mul, add) + N (div)
-    Profiler profiler(timed_runs, warmup_runs);
-    auto check_result = [&] {
-        auto ret = validate_all(h_out.data(), d_out, h_out.size());
-        check_runtime_api(hipMemset(d_out, 0, N * sizeof(float)));
-        return ret;
+    size_t bytes      = 2 * N * sizeof(float);
+    size_t flops      = 3 * N; // 2 * N (mul, add) + N (div)
+    auto check_result = [&] { return validate_all(h_ref.data(), d_out, h_ref.size()); };
+    auto reset_func   = [&] {
+        check_runtime_api(
+            hipMemcpy(d_out, h_data.data(), N * sizeof(float), hipMemcpyHostToDevice));
     };
+    Profiler profiler(timed_runs, warmup_runs, reset_func);
+
     profiler.add(
         "pkNormalize",
         [=]() {
             launchCooperativeKernel((pkNormalize<ThreadsPerBlock>), dim3(NUM_SM),
-                                    dim3(ThreadsPerBlock), 0, 0, d_in, d_out, N, d_reduce_buf);
+                                    dim3(ThreadsPerBlock), 0, 0, d_out, N, d_reduce_buf);
         },
         check_result, bytes, flops);
     profiler.add(
-        "kNrm2-3phase-Vec-1",
-        [=]() { launch_nrm2_multi_phase<true, 1>(d_in, d_out, N, d_reduce_buf); }, check_result,
-        bytes, flops);
+        "kNrm2-3phase-Vec-1", [&] { launch_nrm2_multi_phase<true, 1>(d_out, N, d_reduce_buf); },
+        check_result, bytes, flops);
     profiler.add(
-        "kNrm2-3phase-Vec-2",
-        [=]() { launch_nrm2_multi_phase<true, 2>(d_in, d_out, N, d_reduce_buf); }, check_result,
-        bytes, flops);
+        "kNrm2-3phase-Vec-2", [&] { launch_nrm2_multi_phase<true, 2>(d_out, N, d_reduce_buf); },
+        check_result, bytes, flops);
     profiler.add(
-        "kNrm2-3phase-Vec-2",
-        [=]() { launch_nrm2_multi_phase<true, 4>(d_in, d_out, N, d_reduce_buf); }, check_result,
-        bytes, flops);
+        "kNrm2-3phase-Vec-4", [&] { launch_nrm2_multi_phase<true, 4>(d_out, N, d_reduce_buf); },
+        check_result, bytes, flops);
     RocBlasLv1<float> rocblas;
     profiler.add(
         "rocblas",
         [&]() {
             constexpr auto VecSize = 4;
-            rocblas.nrm2(N, d_in, 1, d_reduce_buf);
-            kSax<ThreadsPerBlock, VecSize>
-                <<<divUp(N, ThreadsPerBlock * VecSize), ThreadsPerBlock>>>(d_in, d_out, N,
-                                                                           d_reduce_buf);
+            float alpha;
+            rocblas.nrm2(N, d_out, 1, &alpha);
+            alpha = 1.0f / alpha;
+            rocblas.scale(N, &alpha, d_out, 1);
         },
         check_result, bytes, flops);
     profiler.runAll();
     {
-        check_runtime_api(hipFree(d_in));
         check_runtime_api(hipFree(d_out));
         check_runtime_api(hipFree(d_reduce_buf));
     }
